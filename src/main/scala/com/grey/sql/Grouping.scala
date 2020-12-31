bee9a82d8574dcb95fcd06cbbeccc66eba6d90ba
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-31 22:21:35 +0000

    Update src/main/scala/com/grey/sql/Grouping.scala

--- src/main/scala/com/grey/sql/Grouping.scala
+++ src/main/scala/com/grey/sql/Grouping.scala
@@ -11,12 +11,18 @@ class Grouping(spark: SparkSession) {
 
     // Volume per month
     spark.sql("SELECT year, month, SUM(volume) as volume " +
-      "FROM stocks GROUP BY year, month ORDER BY year, month").show(11)
+      "FROM stocks GROUP BY year, month ORDER BY year, month").show(5)
 
-    // Yearly
+    // Average of the daily delta values per year
     spark.sql("SELECT year, AVG(close - open) as avg_daily_delta " +
-      "FROM stocks GROUP BY year ORDER BY year").show()
+      "FROM stocks GROUP BY year ORDER BY year").show(5)
 
+    // Extrema of prices per month
+    spark.sql("SELECT year, month, MIN(low) as min_price, MAX(high) as max_price " +
+      "FROM stocks GROUP BY year, month ORDER BY year, month").show(5)
+
+    spark.sql("SELECT year, month, MIN(low) as min_price, MAX(high) as max_price " +
+      "FROM stocks GROUP BY year, month HAVING MAX(high) > 100 ORDER BY year, month").show(5)
 
   }
 
