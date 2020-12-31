ac553bb09dfddb3808441896a09ef7dc73804274
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-31 18:33:01 +0000

    Update src/main/scala/com/grey/sets/Aggregating.scala

--- src/main/scala/com/grey/sets/Aggregating.scala
+++ src/main/scala/com/grey/sets/Aggregating.scala
@@ -2,20 +2,45 @@ package com.grey.sets
 
 import com.grey.metadata.CaseClassOf.Stocks
 import org.apache.spark.sql.{Dataset, SparkSession}
-import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.functions.{count, isnull, sum, min, max, avg}
 
 class Aggregating(spark: SparkSession) {
 
   def aggregating(stocks: Dataset[Stocks]): Unit = {
+
+    // Dataset Approach
+    println("\n\nCase: Dataset")
+
+    // Numeric Fields
+    // val numericFields: Array[Column] = stocks.columns.filterNot(field => field == "date").map(field => col(field))
+    val numericFields: Array[String] = stocks.columns.filterNot(field => field == "date")
 
     // Implicits
     import spark.implicits._
 
     // Counts
     stocks.select(count("*").as("n")).show()
+    stocks.filter(!isnull($"high")).select(count($"high").as("count_of_nonnull_high")).show()
     stocks.select(count($"high").as("count_of_high")).show()
     stocks.select(count($"date").as("count_of_date")).show()
     stocks.summary("count").show()
+
+    // Sums
+    stocks.select(sum($"volume").as("volume")).show()
+
+    val totals = numericFields.map(field => sum(field).as(field))
+    stocks.agg(totals.head, totals.tail: _*).show()
+
+    // Extrema
+    stocks.select(min($"volume").as("min_volume"), max($"volume").as("max_volume")).show()
+
+    stocks.agg(min($"low").as("lowest_price")).show()
+
+    stocks.select(max($"close" - $"open").as("highest_single_day_inc")).show()
+
+    // Averages
+    val averages = numericFields.map(field => avg(field).as("avg_" + field))
+    stocks.agg(averages.head, averages.tail: _*).show()
 
   }
 
