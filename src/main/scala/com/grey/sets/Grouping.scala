98245ee1219f02d2ad8b3d63e1de84b8bfafb860
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-31 22:21:18 +0000

    Update src/main/scala/com/grey/sets/Grouping.scala

--- src/main/scala/com/grey/sets/Grouping.scala
+++ src/main/scala/com/grey/sets/Grouping.scala
@@ -3,7 +3,7 @@ package com.grey.sets
 import com.grey.metadata.CaseClassOf.Stocks
 import org.apache.spark.sql.{Dataset, SparkSession}
 
-import org.apache.spark.sql.functions.{sum, avg}
+import org.apache.spark.sql.functions.{sum, avg, min, max}
 
 class Grouping(spark: SparkSession) {
 
@@ -15,21 +15,27 @@ class Grouping(spark: SparkSession) {
     //Implicits
     import spark.implicits._
 
-
     // Volume per month
     stocks.select($"year", $"month", $"volume").groupBy($"year", $"month")
-      .sum("volume").as("volume").orderBy($"year", $"month").show(11)
+      .sum("volume").alias("volume").orderBy($"year", $"month").show(5)
 
     stocks.select($"year", $"month", $"volume").groupBy($"year", $"month")
-      .agg(sum($"volume").as("volume")).orderBy($"year", $"month").show(11)
+      .agg(sum($"volume").as("volume")).orderBy($"year", $"month").show(5)
 
-
-    // Yearly ...
+    // Average of the daily delta values per year
     stocks.select($"year", ($"close" - $"open").as("daily_delta")).groupBy($"year")
-      .agg(avg($"daily_delta").as("avg_daily_delta")).orderBy($"year").show()
+      .agg(avg($"daily_delta").as("avg_daily_delta")).orderBy($"year").show(5)
 
+    // Extrema of prices per month
+    stocks.select($"year", $"month", $"low", $"high").groupBy($"year", $"month")
+      .agg(min($"low").as("min_price"), max($"high").as("max_price"))
+      .orderBy($"year", $"month").show(5)
 
-
+    // The 'having' proxy
+    stocks.select($"year", $"month", $"low", $"high").groupBy($"year", $"month")
+      .agg(min($"low").as("min_price"), max($"high").as("max_price"))
+      .filter($"max_price" > 100)
+      .orderBy($"year", $"month").show(5)
 
   }
 
