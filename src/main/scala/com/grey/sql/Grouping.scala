acd47ff9d8c069caaca340dad120f995d2735d66
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-31 18:33:54 +0000

    Add src/main/scala/com/grey/sql/Grouping.scala: In progress

---
+++ src/main/scala/com/grey/sql/Grouping.scala
@@ -0,0 +1,19 @@
+package com.grey.sql
+
+import org.apache.spark.sql.SparkSession
+
+class Grouping(spark: SparkSession) {
+
+  def grouping(): Unit = {
+
+    // Programmatic SQL Approach
+    println("\n\nCase: SQL Grouping")
+
+    
+
+
+
+
+  }
+
+}
