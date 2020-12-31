d322ae79200cdcec36aa5ab99f15deced49f4696
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-31 19:36:23 +0000

    Update src/main/scala/com/grey/sql/Grouping.scala

--- src/main/scala/com/grey/sql/Grouping.scala
+++ src/main/scala/com/grey/sql/Grouping.scala
@@ -9,9 +9,9 @@ class Grouping(spark: SparkSession) {
     // Programmatic SQL Approach
     println("\n\nCase: SQL Grouping")
 
-    
-
-
+    // Volume per month
+    spark.sql("SELECT year, month, SUM(volume) as volume " +
+      "FROM stocks GROUP BY year, month ORDER BY year, month").show(11)
 
 
   }
