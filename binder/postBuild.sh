f5f78c8923bf50c8b5cd6bc149fe6b1fb3f43777
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-02 01:36:54 +0000

    Update binder/postBuild.sh

--- binder/postBuild.sh
+++ binder/postBuild.sh
@@ -1,59 +1,49 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-
-# Install coursier
-curl -fLo cs https://github.com/coursier/coursier/releases/download/v2.0.3/cs-x86_64-pc-linux
-chmod +x cs
-
+# Variables
 ALMOND_VERSION=0.10.9
-SCALA_VERSION_PREV=2.11.12 
+
+SCALA_VERSION_PREV=2.11.12
 ALMOND_VERSION_PREV=0.6.0
+
 JUPYTER_CONFIG_DIR=$(jupyter --config-dir)
 
 
+# Install coursier
+curl -Lo coursier https://git.io/coursier-cli
+chmod +x coursier
+
+
+# Install almond for Scala 2.11
+./coursier launch --fork almond --scala 2.11.12 -- --install \
+    --id scala211 \
+    --display-name "Scala (2.11)" \
+    --env "JAVA_OPTS=-XX:MaxRAMPercentage=80.0"
+
+
 # Install almond for Scala 2.13
-./cs launch "almond:${ALMOND_VERSION}" --scala 2.13.3 -- \
-    --install \
+./coursier launch --fork almond --scala 2.13.3 -- --install \
     --id scala213 \
     --display-name "Scala (2.13)" \
-    --env "JAVA_OPTS=-XX:MaxRAMPercentage=80.0" \
-    --variable-inspector \
-    </dev/null 2>&1 | grep -v '^Download'
+    --env "JAVA_OPTS=-XX:MaxRAMPercentage=80.0"
 
 
 # Install almond for Scala 2.12
-./cs launch "almond:${ALMOND_VERSION}" --scala 2.12.12 -- \
-    --install \
+./coursier launch --fork almond --scala 2.12.12 -- --install \
     --id scala212 \
     --display-name "Scala (2.12)" \
-    --env "JAVA_OPTS=-XX:MaxRAMPercentage=80.0" \
-    --variable-inspector \
-    </dev/null 2>&1 | grep -v '^Download'
+    --env "JAVA_OPTS=-XX:MaxRAMPercentage=80.0"
 
 
-# Install almond 0.6.0 for Scala 2.11
-./cs bootstrap \
-    -r jitpack \
-    -i user -I user:sh.almond:scala-kernel-api_${SCALA_VERSION_PREV}:${ALMOND_VERSION_PREV} \
-    sh.almond:scala-kernel_${SCALA_VERSION_PREV}:${ALMOND_VERSION_PREV} \
-    --sources --default=true \
-    -o almond-scala-2.11 \
-    </dev/null 2>&1 | grep -v '^Download'
-    
-./almond-scala-2.11 --install --id scala211 --display-name "Scala (2.11)" \
-    --command "java -XX:MaxRAMPercentage=80.0 -jar almond-scala-2.11 --id scala211 --display-name 'Scala (2.11)'" \
-    --copy-launcher \
-    --metabrowse
-    
-rm -f almond-scala-2.11
+# Hence
+rm -f coursier
 
 
 # Install required Jupyter/JupyterLab extensions
-jupyter labextension install --minimize=False \
-    jupyterlab-plotly \
+jupyter labextension install --minimize=False jupyterlab-plotly \
     @almond-sh/scalafmt \
-    @almond-sh/jupyterlab_variableinspector
+    @almond-sh/jupyterlab_variableinspector \
+    @jupyterlab/toc
 
 
 # Classic notebook
